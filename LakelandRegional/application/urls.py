8c18a7cd7ea212a9a016aec478ef6def59543225
Jillienne Lapid <jillienne.lapid@gmail.com>
2026-04-12 18:45:10 -0400

    Separating changes made by JL to another branch due to being on an older commit.

--- LakelandRegional/application/urls.py
+++ LakelandRegional/application/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("api/carts/", views.get_all_carts, name="get_carts"),
-
+    path("api/filter-carts/", views.filter_carts, name="filter_carts"),
 ]
--- LakelandRegional/application/views.py
+++ LakelandRegional/application/views.py
@@ -7,7 +7,8 @@ from .models import Cart
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
-
+from django.utils import timezone
+from datetime import timedelta
 
 
 # home page view
@@ -51,14 +52,45 @@ def get_all_carts(request):
         'last_seen': str(c.last_seen),
     } for c in carts]})
 
+
+#Emanuel
 @login_required
-def search(request):
-    #TODO
-    pass
+def filter_carts(request):
+    location_id = request.GET.get('location')
+    sort        = request.GET.get('sort')
+    time        = request.GET.get('time')
+
+    carts = Cart.objects.select_related('current_location').all()
+
+    if location_id:
+        carts = carts.filter(current_location__id=location_id)
+    if time == '30':
+        carts = carts.filter(last_seen__gte=timezone.now() - timedelta(minutes=30))
+    elif time == '60':
+        carts = carts.filter(last_seen__gte=timezone.now() - timedelta(hours=1))
+
+    if sort == 'asc':
+        carts = carts.order_by('name')
+    elif sort == 'desc':
+        carts = carts.order_by('-name')
 
 
-@login_required
-def filter():
-    #TODO
-    pass
+    """
+    # Detect AJAX
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'partials/table.html', {'carts': carts})
 
+    return render(request, 'main.html', {'carts': carts})
+
+    """
+    return JsonResponse({
+        'carts': [{
+            'id':           c.id,
+            'name':         c.name,
+            'rfid_tag':     c.rfid_tag,
+            'status_raw':   c.status,               # "in_use"  → used for CSS class
+            'status_label': c.get_status_display(), # "In Use"  → used for display text
+            'location':     c.current_location.name if c.current_location else 'Unknown',
+            'last_seen':    c.last_seen.isoformat() if c.last_seen else None,
+        } for c in carts]
+    })
